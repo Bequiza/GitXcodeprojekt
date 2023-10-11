b036a258ff65d6d4ee62b32a5a31246964a8ed8c
rebecca zadig <rebecca.zadig@gmail.com>
2023-10-11 11:06:44 +0200

    Initial Commit

---
+++ GitXcode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitXcode
+//
+//  Created by Rebecca Zadig on 2023-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hej världen!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitXcode/GitXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXcodeApp.swift
+//  GitXcode
+//
+//  Created by Rebecca Zadig on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct GitXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
